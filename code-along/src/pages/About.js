938b98fb885a1bda48cfaad85178b0b2259a1005
Sandra Aidoo <nanasandra054@gmail.com>
2022-04-04 10:45:01 -0700

    react-component-8

---
+++ code-along/src/pages/About.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section>
+      <h1>Welcome to the About Page</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure, saepe!
+        Enim optio, sunt nesciunt quasi animi, illum incidunt recusandae est
+        modi praesentium repudiandae eligendi, illo minus velit blanditiis
+        laborum natus. Impedit officiis, tempora obcaecati ipsa expedita culpa
+        rerum aliquid aspernatur fuga excepturi accusamus itaque esse aperiam
+        est quam voluptates facilis quisquam? Corporis consequatur magni magnam
+        itaque quia facere. Aspernatur, ducimus. Voluptas cumque modi odit
+        molestiae, quis a accusantium nulla libero doloribus, quaerat possimus
+        consectetur, numquam voluptatibus sapiente placeat vitae. Modi quod
+        impedit minus beatae corrupti reiciendis quae molestias hic delectus.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure, saepe!
+        Enim optio, sunt nesciunt quasi animi, illum incidunt recusandae est
+        modi praesentium repudiandae eligendi, illo minus velit blanditiis
+        laborum natus. Impedit officiis, tempora obcaecati ipsa expedita culpa
+        rerum aliquid aspernatur fuga excepturi accusamus itaque esse aperiam
+        est quam voluptates facilis quisquam? Corporis consequatur magni magnam
+        itaque quia facere. Aspernatur, ducimus. Voluptas cumque modi odit
+        molestiae, quis a accusantium nulla libero doloribus, quaerat possimus
+        consectetur, numquam voluptatibus sapiente placeat vitae. Modi quod
+        impedit minus beatae corrupti reiciendis quae molestias hic delectus.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure, saepe!
+        Enim optio, sunt nesciunt quasi animi, illum incidunt recusandae est
+        modi praesentium repudiandae eligendi, illo minus velit blanditiis
+        laborum natus. Impedit officiis, tempora obcaecati ipsa expedita culpa
+        rerum aliquid aspernatur fuga excepturi accusamus itaque esse aperiam
+        est quam voluptates facilis quisquam? Corporis consequatur magni magnam
+        itaque quia facere. Aspernatur, ducimus. Voluptas cumque modi odit
+        molestiae, quis a accusantium nulla libero doloribus, quaerat possimus
+        consectetur, numquam voluptatibus sapiente placeat vitae. Modi quod
+        impedit minus beatae corrupti reiciendis quae molestias hic delectus.
+      </p>
+    </section>
+  );
+};
+
+export default About;
